33e08cf678372c2ab30485d75b567d25e2a2e466
Pratichi Parida <pratichi_parida@epam.com>
2025-05-13 16:00:17 +0530

    welcome

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=2;
+        int b=4;
+        System.out.println("hello");
+        int sum=a+b;
+        int diff=b-a;
+
+        System.out.println(sum);
+        System.out.println(diff);
+    }
+}
